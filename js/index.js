2ba3e2136a9a01ea063e80b3eff774352928a6db
Yogesh Kumar Bairwa <ybairwa786@gmail.com>
2023-09-17 16:17:39 +0530

    addOn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
-console.log("This is my script");
+let result = {
+  tag: "",
+  free: true,
+  role: false,
+  user: "ybairwa786",
+  email: "ybairwa786@gmail.com",
+  score: 0.64,
+  state: "deliverable",
+  domain: "gmail.com",
+  reason: "valid_mailbox",
+  mx_found: true,
+  catch_all: null,
+  disposable: false,
+  smtp_check: true,
+  did_you_mean: "",
+  format_valid: true,
+};
+
+submitBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  console.log("clicked");
+  let key = " ema_live_A5mvvHBp0lFMSQlg7QTvZHF4TT0PmgiSTwruQL3k";
+  let email = document.getElementById("username").value;
+  let url = `https://api.emailvalidation.io/v1/info?apikey=ema_live_A5mvvHBp0lFMSQlg7QTvZHF4TT0PmgiSTwruQL3k&email=${email}`;
+  let res = await fetch(url);
+  let result = await res.json();
+
+  let str = ``;
+
+  for (key of Object.keys(result)) {
+    if (result[key] !== "" && result[key] !== " ") {
+    }
+    str = str + `<div>${key}:${result[key]}</div>`;
+  }
+  console.log(str);
+  resultCont.innerHTML = str;
+});
